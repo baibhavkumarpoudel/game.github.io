c4ab635e02a471fa018ccdae9adec99196ac5776
Baibhav K Poudel <bkpoudel@up.com>
2021-05-05 13:43:43 -0500

    after pressing submit, page will not refresh

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1 question at a time first
+//break it down
+//addeventlist
+//started with correct answers and turned them into an array for later
+
+const rightAnswers = ['B', 'C', 'C', 'B', 'A', 'A', 'C', 'B', 'A', 'C']
+const GaQuiz = document.querySelector('form')
+
+GaQuiz.addEventListener('submit', function(evt){
+    evt.preventDefault()
+})
